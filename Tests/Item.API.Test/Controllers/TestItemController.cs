08542326995a28a6f2a0dee233b32f7507f11302
Jack Liang <66517361+LIASY032@users.noreply.github.com>
2023-07-18 21:21:12 +1000

    Add Item API Test Startup

--- Tests/Item.API.Test/Controllers/TestItemController.cs
+++ Tests/Item.API.Test/Controllers/TestItemController.cs
@@ -8,20 +8,22 @@ using Moq;
 
 namespace Item.API.Test.Controllers
 {
-	
-	public class TestItemController
+    public class TestItemController
     {
         private Mock< ILogger<ItemController>> _logger;
         private Mock<IProductRepository >_repository;
         private Mock<IMapper >_mapper;
-        [TestInitialize]
-        public void SetUp()
-        {
-        }
+
+        public void Startup() {
+			_repository = new Mock<IProductRepository>();
+			_logger = new Mock<ILogger<ItemController>>();
+			_mapper = new Mock<IMapper>();
+	
+	}
 
         [Fact]
 		public void Get_OnSuccess_GetItem(){
-			var item = new ItemController(_mapper,_repository,_logger );
+			var item = new ItemController(_mapper.Object,_repository.Object,_logger .Object);
 			var result = item.GetItemAsync();
 			result.Status.Should().Be((TaskStatus)200);
 	}
---
+++ Tests/Item.API.Test/Startup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Item.API.Configurations;
+using Item.API.Data;
+using Item.API.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Item.API.Test
+{
+	public class Startup
+	{
+		public Startup()
+		{
+		}
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            //var configuration = new ConfigurationBuilder()
+            //        .SetBasePath(System.IO.Directory.GetCurrentDirectory())
+            //        .AddJsonFile("appsettings.json", false, true)
+            //        .Build();
+
+
+            services.AddAutoMapper(typeof(MapperConfig));
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IItemContext, ItemContext>();
+        }
+    }
+}
+
