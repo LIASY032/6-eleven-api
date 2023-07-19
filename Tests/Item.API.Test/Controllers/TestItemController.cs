7f30edb906ab20c32f3a14b9821bfd0743654892
Jack Liang <66517361+LIASY032@users.noreply.github.com>
2023-07-19 19:03:24 +1000

    Add GetItemAsync test

--- Tests/Item.API.Test/Controllers/TestItemController.cs
+++ Tests/Item.API.Test/Controllers/TestItemController.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Net;
 using AutoMapper;
 using FluentAssertions;
 using Item.API.Controllers;
 using Item.API.Repositories;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
+using Xunit.Abstractions;
 
 namespace Item.API.Test.Controllers
 {
@@ -13,20 +16,22 @@ namespace Item.API.Test.Controllers
         private Mock< ILogger<ItemController>> _logger;
         private Mock<IProductRepository >_repository;
         private Mock<IMapper >_mapper;
-
-        public void Startup() {
+        private readonly ITestOutputHelper output;
+        public TestItemController(ITestOutputHelper output) {
 			_repository = new Mock<IProductRepository>();
 			_logger = new Mock<ILogger<ItemController>>();
 			_mapper = new Mock<IMapper>();
-	
-	}
+            this.output = output;
 
-        [Fact]
-		public void Get_OnSuccess_GetItem(){
+        }
+
+        [Fact(DisplayName = "GET /item returns http status code 200")]
+		public async void Get_OnSuccess_GetItem(){
 			var item = new ItemController(_mapper.Object,_repository.Object,_logger .Object);
-			var result = item.GetItemAsync();
-			result.Status.Should().Be((TaskStatus)200);
-	}
+            var response= await item.GetItemAsync();
+            var result = response.Result as OkObjectResult;
+            result?.StatusCode.Should().Be(200);
+        }
 	}
 }
 
