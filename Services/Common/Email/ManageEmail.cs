aa8475bc4de004a04c3d22e62cf76fff76dd5a70
Jack Liang <66517361+LIASY032@users.noreply.github.com>
2023-12-02 20:25:44 +1100

    Add email service

---
+++ Services/Common/Email/ManageEmail.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+
+namespace Service.Common.Email
+{
+	public class ManageEmail
+	{
+		public void sendEmail(string subject, string email, string body) {
+            // TODO: change address
+            var fromAddress = new MailAddress("from@gmail.com", "From Name");
+            var toAddress = new MailAddress(email, "To Name");
+            const string fromPassword = "fromPassword";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
+        }
+        public void sendConfirmationEmail(string email) {
+            sendEmail("6-Eleven MFA code", email, "");
+	
+	}
+	}
+}
+
